8420da9a322a83de2dec8a0f906747795b59a1b9
Lotzig <lotzig@free.fr>
2025-01-04 07:51:03 +0100

    Retrieve TreeNFT metadata from Pinata gateway and display them in TreeNFTs display : image Ok, other metadata retrieved Ok but displaying to be completed

--- frontend/components/shared/TreeNFT.jsx
+++ frontend/components/shared/TreeNFT.jsx
@@ -1,18 +1,140 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { useReadContract, useAccount } from "wagmi";
+import { nftContractAddress, nftContractAbi } from "@/constants";
 import Image from 'next/image'
 import Accueil from '@/images/Accueil.jpg'
+import { PinataSDK } from "pinata-web3";
+import { useToast } from "../ui/use-toast"; // Toast Shadcn/ui
+
 
 const TreeNFT = ({ treeNFT }) => {
+
+  // Get passed TreeNFT token URI
+  //-----------------------------
+  const { userAddress } = useAccount()
+  const { toast } = useToast() // Toast Shadcn/ui
+  const [tokenURI, setTokenURI] = useState(null);
+
+  // Get TreeNFT token URI from NFT contract
+  const { data: treeNftTokenUri, error: treeNftTokenUriError, isPending: treeNftTokenUriPending
+        , refetch: refetchTreeNftTokenUri } = useReadContract({
+    address: nftContractAddress,
+    abi: nftContractAbi,
+    account: userAddress,
+    functionName: 'tokenURI',
+    args: [ treeNFT.tokenId ],
+  })
+
+  useEffect(() => {
+    // Si erreur récup URI
+    if(treeNftTokenUriError) {
+      toast({
+          title: "TreeNFT token URI fetching error",
+          description: treeNftTokenUriError.shortMessage, 
+          variant: "destructive",
+          status: "error",
+          duration: 4000,
+          isClosable: true,
+      });
+    }
+
+    // Si succès récup URI
+    if(treeNftTokenUri) {
+      setTokenURI(treeNftTokenUri)
+    }
+   
+  }, [treeNftTokenUriError, treeNftTokenUri ])
+
+
+  // Get passed TreeNFT IPFS metadata
+  //----------------------------------
+  const [json, setJson] = useState();
+  const [image, setImage] = useState();
+  const [metadataError, setMetadataError] = useState();
+  const [test, setTest] = useState();
+  const [metadataRetrieved, setmetadataRetrieved] = useState(false);
+
+  useEffect(() => {
+
+    const fetchTreeNFTMetadata = async () => {
+
+      try {
+        const pinata = new PinataSDK({
+          pinataJwt: process.env.NEXT_PUBLIC_PINATA_JWT,
+          pinataGateway: process.env.NEXT_PUBLIC_PINATA_GETWAY,
+        });
+
+        // Get json data from token URI
+        const jsonData = await pinata.gateways.get(treeNftTokenUri);
+        setJson(jsonData);
+console.log("Log jsonData")
+console.log(jsonData)
+
+        // Get TreeNFT image from retrieved json attribute
+        const img = await pinata.gateways.get(jsonData.data.image);
+        setImage(URL.createObjectURL(img.data));
+
+console.log("Log img")
+console.log(img)
+console.log("Log URL.createObjectURL(img.data)")
+console.log(URL.createObjectURL(img.data))
+
+      } catch (err) {
+        console.error(err);
+        setMetadataError(err);
+      }
+    };
+
+    if (! metadataRetrieved && treeNftTokenUri) {
+
+      fetchTreeNFTMetadata();
+      setmetadataRetrieved(true)
+
+    }
+
+  }, [treeNftTokenUri]);
+
+
+  useEffect(() => {
+
+    // Si erreur récup metadata
+    if(metadataError) {
+      toast({
+        title: "TreeNFT metadata fetching error",
+        description: metadataError.shortMessage, 
+        variant: "destructive",
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
+    }
+   
+  }, [ metadataError ])
+
+
+  useEffect(() => {
+    console.log("État json mis à jour :", json);
+  }, [json]);
+
 
   return (
     <>
     {
       <div className="flex flex-none items-center font-bold pb-10">
-          <Image
-                  src={Accueil}
-                  alt="TreesNFT"
-                  width={500} 
-                  height={500}
-          />
+
+          {image ? (
+              <Image
+                      src={image}
+                      alt="TreesNFT image"
+                      width={500} 
+                      height={500}
+              />
+              ) : (
+                <p>Image uploading...</p>
+            )
+          }
 
           <div className="pl-10">
             <ul>
@@ -22,7 +144,7 @@ const TreeNFT = ({ treeNFT }) => {
               <li>Seller : {treeNFT.seller}</li>
               <li>Owner : {treeNFT.owner}</li>
               <li>For Sale : {treeNFT.forSale.toString()}</li>
-              <li>Species : </li>
+              <li>Species : {json ? (json.data.name):("")}</li>
               <li>Planting Date : </li>
               <li>Location : </li>
               <li>Location Owner Name : </li>
